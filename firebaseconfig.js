ee1801921bb7a18fea39a7cb1c7fea2f433c3b29
ZhaoBin Li <173764296+bluija@users.noreply.github.com>
2025-04-14 16:49:58 -0700

    Add files via upload

---
+++ firebaseconfig.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDcQ5R_uXrvZefV1DQV53V2Ho_zhGOi3t4",
+    authDomain: "ppqq-a3255.firebaseapp.com",
+    projectId: "ppqq-a3255",
+    storageBucket: "ppqq-a3255.firebasestorage.app",
+    messagingSenderId: "461593688932",
+    appId: "1:461593688932:web:f48ab5dd41c51afb455871"
+};
